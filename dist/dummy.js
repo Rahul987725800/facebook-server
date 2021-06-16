90c63f8deeb80d0dfc7fa188482670d39ca77175
Rahul Gupta <guptarahul70322@gmail.com>
2021-06-16 14:21:06 +0530

    removied

--- dist/dummy.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-function config() {
-    console.log('config ran');
-}
-exports.config = config;
-config();
-//# sourceMappingURL=dummy.js.map
