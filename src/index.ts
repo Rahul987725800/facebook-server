622d8fedf76b7b36076ee34119abd633fbae700b
Rahul Gupta <guptarahul70322@gmail.com>
2021-06-19 14:55:54 +0530

    image upload multer to cloudinary

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,19 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
 import { ApolloServer } from 'apollo-server-express';
 import mongoose from 'mongoose';
 import { schema } from './Schema';
 import socketHelper from './helpers/socket';
 import { createMessage } from './functions/message';
-import uploadFile from './helpers/uploadFile';
-import Asset from './models/Asset';
+
 const app = express();
 app.use(cors());
 app.get('/', (_req, res) => {
   res.send('<h1>Connected visit please visit please /graphql</h1>');
 });
 // console.log(__dirname);
-app.use('/images', express.static(path.join(__dirname, '../images')));
 
-app.post('/assets', uploadFile, async (req, res) => {
-  if (!req.file) {
-    console.log('no image');
-    return res.json({
-      error: true,
-      message: 'Image not provided',
-    });
-  }
-  const imageUrl = req.file.path.replace('\\', '/');
-  try {
-    const createdAsset = await new Asset({
-      type: 'image',
-      value: imageUrl,
-    });
-    createdAsset.save();
-    return res.status(201).json({
-      message: 'Asset created successfully',
-      asset: createdAsset,
-    });
-  } catch (err) {
-    return res.json({
-      error: true,
-      message: err.message,
-    });
-  }
-});
 const apolloServer = new ApolloServer({
   schema: schema,
   context: ({ req, res }) => ({
